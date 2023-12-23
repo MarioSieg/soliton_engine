5fb5d1c6f8df8a1d43fc988d2e28ae7e36f380a4
Neo <mario.sieg.64@gmail.com>
2023-12-23 17:27:21 +0100

    scripting

--- media/scripts/__boot.lua
+++ media/scripts/__boot.lua
@@ -1,3 +1,23 @@
 -- Copyright (c) 2022-2023 Mario "Neo" Sieg. All Rights Reserved.
 
-print('Hello from Lua!')
+local INCLUDE_DIRS = {
+    'media/scripts',
+    'media/scripts/lib'
+}
+
+-- add all other directories to package paths
+for _, path in ipairs(INCLUDE_DIRS) do
+    print('Additional Lua library dirs: ' .. path)
+    package.path = string.format('%s;%s/?.lua', package.path, path)
+end
+
+-- print JIT info
+local jit = require 'jit'
+print(jit.version)
+print(jit.os)
+print(jit.arch)
+local inspect = require 'inspect'
+print('JIT active: '..inspect({jit.status()}))
+
+-- Uncomment to enable JIT assembly dump (useful for optimizing)
+-- require('jit.dump').on('m')
