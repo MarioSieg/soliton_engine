9ca5af4099714118102a4b39241164fe7a73f8da
Neo <mario.sieg.64@gmail.com>
2024-04-17 00:16:54 +0200

    Update vdrawhelper_neon.cpp

--- extern/rlottie/src/vector/vdrawhelper_neon.cpp
+++ extern/rlottie/src/vector/vdrawhelper_neon.cpp
@@ -14,7 +14,10 @@ extern "C" void pixman_composite_over_n_8888_asm_neon(int32_t w, int32_t h,
 
 void memfill32(uint32_t *dest, uint32_t value, int length)
 {
-    pixman_composite_src_n_8888_asm_neon(length, 1, dest, length, value);
+    //pixman_composite_src_n_8888_asm_neon(length, 1, dest, length, value);
+    for (int i = 0; i < length; i++) {
+        dest[i] = value;
+    }
 }
 
 static void color_SourceOver(uint32_t *dest, int length,
@@ -23,7 +26,11 @@ static void color_SourceOver(uint32_t *dest, int length,
 {
     if (const_alpha != 255) color = BYTE_MUL(color, const_alpha);
 
-    pixman_composite_over_n_8888_asm_neon(length, 1, dest, length, color);
+    //pixman_composite_over_n_8888_asm_neon(length, 1, dest, length, color);
+
+    for (int i = 0; i < length; i++) {
+        dest[i] = color;
+    }
 }
 
 void RenderFuncTable::neon()
