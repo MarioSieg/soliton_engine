430ac20ba935a17bb0e6081c019eab1abebaed6f
Neo <mario.sieg.64@gmail.com>
2024-09-07 18:31:13 +0200

    audio components

---
+++ src/scene/components/audio_source.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 Mario "Neo" Sieg. All Rights Reserved.
+
+#include "audio_source.hpp"
+
+namespace lu::com {
+
+}
---
+++ src/scene/components/audio_source.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Mario "Neo" Sieg. All Rights Reserved.
+
+#pragma once
+
+#include "../base.hpp"
+
+namespace lu::com {
+    struct audio_source final {
+
+    };
+}
